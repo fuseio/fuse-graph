18f428a16aeb5572d234c6a7274f5e8a7bf688fa
Leon Prouger <leonprou@gmail.com>
2020-02-09 12:33:05 +0200

    Adding timestamps and startBlock

--- fuse-subgraph/src/mappings/wallet.ts
+++ fuse-subgraph/src/mappings/wallet.ts
@@ -7,9 +7,10 @@ import { Wallet } from "../../generated/schema"
 export function handleWalletCreated(event: WalletCreated): void {
   let wallet = new Wallet(event.params._wallet.toHexString())
   wallet.blockNumber = event.block.number
+  wallet.timestamp = event.block.timestamp.toI32()
   wallet.txHash = event.transaction.hash
   wallet.address = event.params._wallet
   wallet.owner = event.params._owner
-
+  
   wallet.save()
 }
